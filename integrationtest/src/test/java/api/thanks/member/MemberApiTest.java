252597b92454d204f4d12a7f60e37c931d8926df
samitkumar patel <samitkumar.patel@maersk.com>
2018-06-29 15:01:03 +0200

    integration test spac added

---
+++ integrationtest/src/test/java/api/thanks/member/MemberApiTest.java
@@ -0,0 +1,14 @@
+package api.thanks.member;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MemberApiTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
